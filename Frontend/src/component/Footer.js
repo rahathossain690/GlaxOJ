caffb84a79b5a53478d621c71647dbdee502f931
Rahat Hossain <rahathossain690@gmail.com>
2021-03-28 16:19:58 +0600

    github link

--- Frontend/src/component/Footer.js
+++ Frontend/src/component/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-blue-600 mb-3">
             <div className="px-4 mx-auto flex flex-wrap item-center justify-between">
-               <span className="text-white"> GlaxOJ@2021 | <Link to="github">Github</Link> </span>
+               <span className="text-white"> GlaxOJ@2021 | <a href="https://github.com/rahathossain690/GlaxOJ" target="_blank">Github</a> </span>
             </div>
     </footer>
   );
